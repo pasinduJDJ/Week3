fdf54b05a2b60052848b1a04535679e92b84145e
pasinduJDJ <pasindudulanjanarj@gmail.com>
2025-05-30 15:01:04 +0530

    Add New User, add Some Posts

--- Day01/mysite/home/models.py
+++ Day01/mysite/home/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class Post(models.Model):  
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     content = models.TextField()
+    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
